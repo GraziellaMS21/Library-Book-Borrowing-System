8d8d96f517d502ce3f4f40aa2fb6e74856321b3a
GraziellaMS21 <graziellamssaavedra06@gmail.com>
2025-10-07 21:28:01 +0800

    Add Register Responsiveness and Improved Design

--- borrowbook/addUser.php
+++ borrowbook/addUser.php
@@ -93,82 +93,136 @@
     <title>Register Account</title>
     <link rel="stylesheet" href="../borrowbook/css/addUser.css"/>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Licorice&display=swap" rel="stylesheet">
 </head>
-<body class="h-screen flex justify-center items-center">
+<body class="h-screen">
     <div class="color-layer"></div>
+        <nav class="navbar flex justify-between items-center bg-white fixed top-0 left-0 w-full z-10">
+            <div class="logo-section flex items-center gap-3">
+                <img src="../borrowbook/images/logo.png" alt="Logo" class="logo">
+                <h2 class="title">WMSU LIBRARY</h2>
+            </div>
 
-    <div class="form-container flex justify-center">
-        <div class="info-section w-1/2 flex flex-col justify-center items-center">
-            <img src="../borrowbook/images/university_library_bg.png" alt="" class="image">
-        </div>
-        
-        <div class="form-section w-1/2 flex flex-col justify-center items-center">
-            <h1 class="text-3xl font-extrabold">REGISTER YOUR ACCOUNT</h1>
-                <form action="" method="POST">
-                    <div class="borrowerType">
-                        <label for="borrowerType">Register as? <span>*</span></label>
-                        <select name="borrowerTypeID" id="borrowerType" onchange="borrowerType()" class="h-12 w-full rounded-lg">
-                            <option value="">--Select--</option>
-                            <?php
-                                forEach($borrowerTypes as $type){
-                            ?>
-                                <option value="<?= $type["id"]?>" <?= isset($register["borrowerTypeID"])  && $register["borrowerTypeID"] == $type["id"] ? "selected" : "" ?> ><?= $type["typeName"]?></option>
-                            <?php
-                                }
-                            ?>
-                        </select>
-                        <p class="errors"><?= $errors["borrowerTypeID"] ?? ""?></p>
-                    </div>
-                    
-                    <div class="input">
-                        <label for="lName">Last Name <span>*</span> : </label>
-                        <input type="text" class="input-field" name="lName" id="lName" value="<?= $register["lName"] ?? "" ?>">
-                        <p class="errors"><?= $errors["lName"] ?? "" ?></p>
-                    </div>
+            <ul class="nav-links flex gap-8 list-none">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+
+            <div class="burger flex flex-col justify-between cursor-pointer">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </nav>
+    </header>
+
+    <main class="flex justify-center items-center">
+        <div class="form-container flex justify-center">
+            <div class="info-section w-1/2 flex flex-col justify-center items-center">
+                <div class="image">
+                    <img src="../borrowbook/images/bg.png" alt="">
+                </div>
+            </div>
             
-                    <div class="input">
-                        <label for="fName">First Name<span>*</span> : </label>
-                        <input type="text" class="input-field" name="fName" id="fName" value="<?= $register["fName"] ?? "" ?>">
-                        <p class="errors"><?= $errors["fName"] ?? ""?></p>
-                    </div>
-                    
-                    <div class="input">
-                        <label for="middleIn">Middle Initial : </label>
-                        <input type="text" class="input-field" name="middleIn" id="middleIn" value="<?= $register["middleIn"] ?? "" ?>">
-                        <p class="errors"><?= $errors["middleIn"] ?? ""?></p>
-                    </div>
-                    <div class="input">
-                        <label for="contactNo">Contact Number<span>*</span> : </label>
-                        <input type="text" class="input-field" name="contactNo" id="contactNo" value="<?= $register["contactNo"] ?? "" ?>">
-                        <p class="errors"><?= $errors["contactNo"] ?? ""?></p>
-                    </div>
-            
-                    <div class="input">
-                        <label for="email">Email<span>*</span> : </label>
-                        <p id = "emailMessage" >Hello</p>
-                        <input type="text" class="input-field" name="email" id="email" value="<?= $register["email"] ?? "" ?>">
-                        <p class="errors"><?= $errors["email"] ?? ""?></p>
-                    </div>
-            
-                    <div class="input">
-                        <label for="password">Password<span>*</span> : </label>
-                        <input type="text" class="input-field" name="password" id="password" value="<?= $register["password"] ?? "" ?>">
-                        <p class="errors"><?= $errors["password"] ?? ""?></p>
-                    </div>
-            
-                    <div class="input">
-                        <label for="conPass">Confirm Password<span>*</span> : </label>
-                        <input type="text" class="input-field" name="conPass" id="conPass" value="<?= $register["conPass"] ?? "" ?>">
-                        <p class="errors"><?= $errors["conPass"] ?? ""?></p>
-                    </div>
-                    
-                    <br>
-                    <input type="submit" value="Register Account" class="font-bold cursor-pointer mb-8 border-none rounded-lg">
-                </form>
+            <div class="form-section w-1/2 flex flex-col justify-center items-center">
+                <h1 class="font-extrabold">REGISTER YOUR ACCOUNT</h1>
+                    <form action="" method="POST">
+                        <div class="borrowerType">
+                            <label for="borrowerType">Register as? <span>*</span></label>
+                            <select name="borrowerTypeID" id="borrowerType" onchange="borrowerType()" class="h-12 w-full rounded-lg">
+                                <option value="">--Select--</option>
+                                <?php
+                                    forEach($borrowerTypes as $type){
+                                ?>
+                                    <option value="<?= $type["id"]?>" <?= isset($register["borrowerTypeID"])  && $register["borrowerTypeID"] == $type["id"] ? "selected" : "" ?> ><?= $type["typeName"]?></option>
+                                <?php
+                                    }
+                                ?>
+                            </select>
+                            <p class="errors"><?= $errors["borrowerTypeID"] ?? ""?></p>
+                        </div>
+                        
+                        <div class="input">
+                            <label for="lName">Last Name <span>*</span> : </label>
+                            <input type="text" class="input-field" name="lName" id="lName" value="<?= $register["lName"] ?? "" ?>">
+                            <p class="errors"><?= $errors["lName"] ?? "" ?></p>
+                        </div>
+                
+                        <div class="input">
+                            <label for="fName">First Name<span>*</span> : </label>
+                            <input type="text" class="input-field" name="fName" id="fName" value="<?= $register["fName"] ?? "" ?>">
+                            <p class="errors"><?= $errors["fName"] ?? ""?></p>
+                        </div>
+                        
+                        <div class="input">
+                            <label for="middleIn">Middle Initial : </label>
+                            <input type="text" class="input-field" name="middleIn" id="middleIn" value="<?= $register["middleIn"] ?? "" ?>">
+                            <p class="errors"><?= $errors["middleIn"] ?? ""?></p>
+                        </div>
+                        <div class="input">
+                            <label for="contactNo">Contact Number<span>*</span> : </label>
+                            <input type="text" class="input-field" name="contactNo" id="contactNo" value="<?= $register["contactNo"] ?? "" ?>">
+                            <p class="errors"><?= $errors["contactNo"] ?? ""?></p>
+                        </div>
+                
+                        <div class="input">
+                            <label for="email">Email<span>*</span> : </label>
+                            <p id = "emailMessage" >Hello</p>
+                            <input type="text" class="input-field" name="email" id="email" value="<?= $register["email"] ?? "" ?>">
+                            <p class="errors"><?= $errors["email"] ?? ""?></p>
+                        </div>
+                
+                        <div class="input">
+                            <label for="password">Password<span>*</span> : </label>
+                            <input type="text" class="input-field" name="password" id="password" value="<?= $register["password"] ?? "" ?>">
+                            <p class="errors"><?= $errors["password"] ?? ""?></p>
+                        </div>
+                
+                        <div class="input">
+                            <label for="conPass">Confirm Password<span>*</span> : </label>
+                            <input type="text" class="input-field" name="conPass" id="conPass" value="<?= $register["conPass"] ?? "" ?>">
+                            <p class="errors"><?= $errors["conPass"] ?? ""?></p>
+                        </div>
+                        
+                        <br>
+                        <input type="submit" value="Register Account" class="font-bold cursor-pointer mb-8 border-none rounded-lg">
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
+    </main>
+
+    <footer class="footer mt-12 text-white">
+        <div class="footer-container flex flex-col md:flex-row justify-between items-center gap-4 px-8 py-6">
+            <div class="footer-left text-center md:text-left">
+                <h3 class="text-xl font-bold tracking-wide">WMSU Library System</h3>
+                <p class="line opacity-90">Home of a Wealthy Knowledge</p>
+            </div>
+
+            <div class="footer-center flex gap-6 text-sm font-semibold">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </div>
+
+            <div class="footer-right text-sm text-center md:text-right opacity-90">
+                <p>©2025 WMSU Library. All Rights Reserved.</p>
+            </div>
+        </div>
+    </footer>
+
 </body>
+<script>
+  const burger = document.querySelector('.burger');
+  const navLinks = document.querySelector('.nav-links');
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+  });
+</script>
 <script>
      document.addEventListener('DOMContentLoaded', function () {
     const select = document.getElementById('borrowerType');
@@ -188,7 +242,6 @@
       }
     }
 
-    // attach listener and run once on load (handles preselected option)
     select.addEventListener('change', updateEmailMessage);
     updateEmailMessage();
   });
