c8f9591b54d5f5e948104af2c96ae97370524b88
GraziellaMS21 <graziellamssaavedra06@gmail.com>
2025-10-09 17:28:26 +0800

    Add Feature in Dashboard

--- app/views/librarian/dashboard.php
+++ app/views/librarian/dashboard.php
@@ -1,8 +1,8 @@
 <?php
   session_start();
   if (!isset($_SESSION["user_id"])) {
-      header("Location: ../../app/views/borrower/login.php");
-      exit;
+    header("Location: ../../app/views/borrower/login.php");
+    exit;
   }
 ?>
 
